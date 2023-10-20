8d333b8ccf550e32da3314736d355e417ec0b9ab
Lucas Moreira Nunes <lucas2005dev@gmail.com>
2023-10-20 10:51:33 -0300

    feature: style de cards e criação de css global para cada tipo de pokemon.

--- src/app/home/card-pokemon/card-pokemon.component.ts
+++ src/app/home/card-pokemon/card-pokemon.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CardPokemonComponent  implements OnInit {
 
   @Input() pokemonName: string = "";
+  @Input() pokemonType: string = "";
   @Input() pokemonImg: string = "";
 
   constructor() { }
