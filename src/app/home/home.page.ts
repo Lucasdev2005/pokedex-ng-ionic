195e1cf94e536dd22796ff84539b4c89f033fc12
Lucas Moreira Nunes <lucas2005dev@gmail.com>
2023-10-20 09:58:40 -0300

    feature: paginação de dados.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,17 +8,29 @@ import { PokemonService } from '../services/pokemon.service';
   providers: [PokemonService]
 })
 export class HomePage {
-  public pokemons: any = []; 
+  public pokemons: any = [];
+  public currentPage: number = 0;
+
   constructor(public pokemon: PokemonService) {
-    this.loadPokemons(0);
+    this.loadPokemons(this.currentPage);
   }
 
   async loadPokemons(currentPage: number) {
+    this.pokemons = [];
     this.pokemon.listPokemons(currentPage).subscribe({
       next: (response: any) => {
         this.pokemons = response;
-        console.log("this.pokemons", this.pokemons);
       }
     });
   }
+
+  paginateTo(goTo: "left" | "right") {
+    if (goTo == "right") {
+      this.currentPage += 6;
+    }
+    else if (goTo == "left" && this.currentPage != 0) {
+      this.currentPage -= 6;
+    }
+    this.loadPokemons(this.currentPage);
+  };
 }
